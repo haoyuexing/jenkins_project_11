e7a677b99dcbc63ad26ea273aaf4a2040e24cee6
haoyuexing <haoyuexing@gmail.com>
2021-02-05 09:33:09 +0800

    hello

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -8,14 +8,3 @@ class TestLogin:
 
     def test_hello3(self):
         assert True
-
-    def test_hello4(self):
-        assert False
-
-
-    def test_hello5(self):
-        assert False
-
-
-    def test_hello6(self):
-        assert False
